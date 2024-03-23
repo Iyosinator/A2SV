e48f72b90e757d462cc5a27150d51248776faa77
Eyosias Mulugeta <iyosiasmulugeta@gmail.com>
2024-03-23 13:44:16 +0300

    Arthemetic Operation

---
+++ Arithmetic Operators/Arithmetic Operators_HackerRank.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input()) 
+    b = int(input())
+    sum_result = a+b
+    difference_result = a-b
+    product_result = a*b
+    print (sum_result)
+    print(difference_result)
+    print(product_result)
