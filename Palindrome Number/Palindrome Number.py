8dcfe6fb56814b73b8334903360edde3146e2bd3
Eyosias Mulugeta <iyosiasmulugeta@gmail.com>
2024-04-03 14:15:34 +0300

    Modified

--- Palindrome Number/Palindrome Number.py
+++ Palindrome Number/Palindrome Number.py
@@ -8,15 +8,9 @@
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x = str(x)
-        if( x == x[::-1]):
-            return True
-        else:
-            return False
-    
+        return str(x) == str(x)[::-1]
     
 # Time complexity: O(n)
 # Space complexity: O(1)
-# Runtime: 56 ms
-    
+        
         
