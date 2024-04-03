cafd7b095a8e574e644f77ed72e046681ed95188
Eyosias Mulugeta <iyosiasmulugeta@gmail.com>
2024-04-03 14:45:57 +0300

    Smallest Even Multiple

---
+++ Smallest Even Multiple/Smallest Even Mutiple.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/smallest-even-multiple/
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n % 2 == 0:
+            return n
+        else:
+            return 2*n
+        
