6df962443fe0f6a004bd2192d058895591d57f1c
Eyosias Mulugeta <iyosiasmulugeta@gmail.com>
2024-09-01 15:07:41 +0300

    9. Palindrome Number by Converting String

--- Palindrome Number/Palindrome Number.py
+++ Palindrome Number/Palindrome Number.py
@@ -1,16 +1,10 @@
 #https://leetcode.com/problems/palindrome-number/description/
 
-# How to solve it:
-# 1. Convert the number to a string
-# 2. Compare the string with its reverse
-# 3. If they are the same, return True
-# 4. Otherwise, return False
-
+# By Converting String
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         return str(x) == str(x)[::-1]
-    
-# Time complexity: O(n)
-# Space complexity: O(1)
+
+
         
         
